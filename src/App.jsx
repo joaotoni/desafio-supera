2858e9dfb6e8c9fdd376809da28644f8f3989a4a
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-03-07 19:27:35 -0300

    Header finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./Components/Header/Header"
 import "./Styles/Global.css"
 
 function App() {
   return(
     <div className="bg-black">
-
+        <Header />
     </div>
   )
 }
