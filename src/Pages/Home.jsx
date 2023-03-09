1df28596bf6ca1e50ad0f1525683be5fdca36c84
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-03-08 22:18:10 -0300

    I almost finish Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import Card from "../Components/Card/Card";
-import Footer from "../Components/Footer/Footer";
-import Header from "../Components/Header/Header";
+
 
 
 export default function Home(){
