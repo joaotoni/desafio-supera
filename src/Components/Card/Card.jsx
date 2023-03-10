d9b1bc37a5bdc9d02ac742e7e91a56afa54f61f5
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-03-10 20:34:43 -0300

    Starting the next logic

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,8 @@
-import List from "../../products.json"
-import List2 from "../../products2.json"
 import { useContext } from "react";
 import { CartContext } from "../../Context/CartContext/CartContext";
 
 export default function Card({info}){
-
+    const { addProducToCart } = useContext(CartContext);
     return(
         <div className="p-4 mx-4 bg-[#b2b4b4] flex flex-wrap items-center justify-center text-center rounded-[12px]" >
             <div>
@@ -12,8 +10,8 @@ export default function Card({info}){
             </div>
             <div className="w-[250px]">
                 <h2 className="text-[#3299f5] text-[28px]">{info.name}</h2>
-                <p className="text-[#044d79] text-[22px]">Pontuação: {info.score}</p>
-                <p className="text-[#044d79] text-[22px]">R${info.price}</p>
+                <p className="text-[#044d79] text-[22px]"> pontuação: {info.score}</p>
+                <p className="text-[#044d79] text-[22px]">Esse jogo está apenas por R${info.price}</p>
                 <button className="text-[#0495f0] bg-[#7ccbfc] p-2 rounded-xl text-[22px] mt-2" onClick={() => addProducToCart(info)}>Adicionar no carrinho</button>  
             </div>
         </div>
